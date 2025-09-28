3374075e49346ab56681a7571e938bd51b84e61e
Abhinav Pillai <abhinavpillai92@gmail.com>
2025-09-28 13:51:12 +0530

    trying bg-charcoal

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-charcoal text-white">
+      {/* Your homepage content will go here */}
+      <div className="container mx-auto px-4 py-12">
+        <h1 className="text-4xl font-bold mb-6">PlusTech Systems & Solutions</h1>
+        <p className="text-xl mb-8">Industrial Finishing Excellence</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
